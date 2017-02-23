2e8a6d76edef8be50d0495305f55fcdc318ca707
chiyingleung1 <chiying.leung@gmail.com>
2017-02-23 14:54:14 -0500

    Add before_destroy callback to delete signups when player is deleted

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,6 +4,7 @@ class Player < ActiveRecord::Base
   has_many :signed_up_practices, through: :signups, source: :practice
   # can call Player.last.signed_up_practices. Source refers to the belongs_to association in signups,
   # which should correspond to practice (we want to access the practices)
+  before_destroy :destroy_practice_signups
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -13,6 +14,11 @@ class Player < ActiveRecord::Base
   def signed_up?(practice)
     # signed_up_practices = signups.collect(:&practice)
     return signed_up_practices.include?(practice)
+  end
+
+  # delete signups associated with Player if Player is destroyed
+  def destroy_practice_signups
+    self.signups.delete_all
   end
 
 end
