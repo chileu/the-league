9f61bc875aacdfda0a25ec1ff50f318a8ffc4129
chiyingleung1 <chiying.leung@gmail.com>
2017-02-24 14:34:51 -0500

    Add seed data in environment variable for players on roster

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-players_on_roster = [
-  ["Andrew Giuliani"],
-  ["Dale Vallaincourt"]
-]
-
-players_on_roster.each do |player|
-  first_name = player[0].split.first
-  last_name = player[0].split.last
-  Player.create(first_name: last_name, last_name: last_name)
+YAML.load(ENV["PLAYERS_ON_ROSTER"]).each do |player|
+    new_player = Player.create!(first_name: player[0].split.first,
+                                last_name: player[0].split.last,
+                                email: player[1],
+                                password: ENV["PASSWORD"])
+    if new_player.first_name == "Chi-Ying" || new_player.last_name == "Xu"
+      new_player.captain = true
+      new_player.save!
+    end
 end
+
+# password = ENV["PASSWORD"]
+#
+# players_on_roster.each do |player|
+#   first_name = player[0].split.first
+#   last_name = player[0].split.last
+#   email = player[1]
+#   Player.create!(first_name: first_name, last_name: last_name, email: email, password: password)
+# end
